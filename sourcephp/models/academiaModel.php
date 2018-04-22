d9b20f79019fde859aa241551971da17901dcfe7
Violinistae <ssbbemims@gmail.com>
2018-04-21 20:41:52 -0500

    medio agregados algunos modelos

---
+++ sourcephp/models/academiaModel.php
@@ -0,0 +1,14 @@
+<?php
+    class academiaModel {
+        private $Id_Academia;
+        private $Academia;
+        private $Clave_Accceso;
+        private $Ciclo_Periodo;
+        private $Lista_Prof;
+        private $Coordinador_Acad;
+        private $Carrera;
+        
+
+    }
+    
+?>
